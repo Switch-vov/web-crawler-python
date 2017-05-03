b73d612ad4440922f4d9ceb2c44f71f2c5920450
Laughing_Sir <824419569@qq.com>
2017-05-03 09:14:12 +0800

    fix:修复商品id为数值型的情况

--- 17cai/downloadMainPicture.py
+++ 17cai/downloadMainPicture.py
@@ -5,17 +5,17 @@ import os
 MAIN_PICTURE_FILE_PATH = 'resources/主图信息.xlsx'
 HTTP = 'http://'
 DOMAIN_NAME = 'http://www.17cai.com'
-DIR_PATH = "J:/齐采网/"
+DIR_PATH = "G:/齐采网/"
 
 os.makedirs(DIR_PATH, exist_ok=True)
 workbook = openpyxl.load_workbook(MAIN_PICTURE_FILE_PATH)
 main_picture_sheet = workbook.get_active_sheet()
 for row in range(2, main_picture_sheet.max_row + 1):
-    product_id = main_picture_sheet.cell(row=row, column=1).value
+    product_id = str(main_picture_sheet.cell(row=row, column=1).value)
     large_image_url = main_picture_sheet.cell(row=row, column=3).value
     if not large_image_url.startswith(HTTP):
         large_image_url = DOMAIN_NAME + large_image_url
-    print(str(product_id) + ":" + large_image_url)
+    print(product_id + ":" + large_image_url)
 
     base_path = os.path.join(DIR_PATH, product_id)
     os.makedirs(base_path, exist_ok=True)
